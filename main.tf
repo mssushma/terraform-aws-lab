3d9978b6911f910526403fc57592dfb924c49989
mssushma <87899596+mssushma@users.noreply.github.com>
2026-07-28 17:50:10 +0530

    Created main.tf

---
+++ main.tf
@@ -0,0 +1,139 @@
+provider "aws" {
+  region = var.region
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "terraform-vpc"
+  }
+}
+
+# Public Subnet
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# Private Subnet
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+# Internet Gateway
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "terraform-igw"
+  }
+}
+
+# Route Table
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Security Group
+
+resource "aws_security_group" "ec2_sg" {
+
+  name   = "terraform-ssh-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol = "-1"
+
+    from_port = 0
+    to_port   = 0
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2
+resource "aws_instance" "server" {
+
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+
+  subnet_id = aws_subnet.public.id
+
+  vpc_security_group_ids = [
+    aws_security_group.ec2_sg.id
+  ]
+
+  user_data = <<-EOF
+              #!/bin/bash
+
+              yum update -y
+
+              yum install -y httpd
+
+              systemctl start httpd
+              systemctl enable httpd
+
+              echo "<h1>Created by Terraform via GitHub Actions</h1>" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "github-actions-ec2"
+  }
+}
